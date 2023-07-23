af83d8c4d1df8732bec80ce1ae96325b96d00d79
cjrcjrdl1 <cjrcjrdl1@naver.com>
2023-07-23 14:30:18 +0900

    domain - posts

---
+++ min/src/test/java/com/blog/min/MinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.min;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
