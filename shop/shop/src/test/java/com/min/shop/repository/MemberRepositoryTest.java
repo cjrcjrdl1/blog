3ed2f9e7723d59253f1088e8ef0c72348d1ffba6
cjrcjrdl1 <cjrcjrdl1@naver.com>
2023-08-13 21:26:41 +0900

    MemberRepository Development

--- shop/shop/src/test/java/com/min/shop/repository/MemberRepositoryTest.java
+++ shop/shop/src/test/java/com/min/shop/repository/MemberRepositoryTest.java
@@ -15,20 +15,20 @@ class MemberRepositoryTest {
     @Autowired
     MemberRepository memberRepository;
 
-    @Test
-    @Transactional
-    @Rollback(value = false)
-    public void testMember() {
-        Member member = new Member();
-        member.setUsername("memberA");
-        Long savedId = memberRepository.save(member);
-
-        Member findMember = memberRepository.find(savedId);
-
-        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
-
-        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
-
-        Assertions.assertThat(findMember).isEqualTo(member);
-    }
+//    @Test
+//    @Transactional
+//    @Rollback(value = false)
+//    public void testMember() {
+//        Member member = new Member();
+//        member.setUsername("memberA");
+//        Long savedId = memberRepository.save(member);
+//
+//        Member findMember = memberRepository.find(savedId);
+//
+//        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+//
+//        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+//
+//        Assertions.assertThat(findMember).isEqualTo(member);
+//    }
 }
