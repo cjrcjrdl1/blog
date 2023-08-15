e0a9266c1cd14b9fa0ff9f458f8ea9dc1b8222cd
cjrcjrdl1 <cjrcjrdl1@naver.com>
2023-08-15 20:45:10 +0900

    OrderSearch Development

---
+++ shop/shop/src/main/java/com/min/shop/domain/OrderSearch.java
@@ -0,0 +1,13 @@
+package com.min.shop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OrderSearch {
+
+    private String memberName;
+    private OrderStatus orderStatus; //주문 상태[ORDER, CANCEL]
+
+}
