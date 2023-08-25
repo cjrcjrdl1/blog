2b100f2c0b2a7db4ed738db11c8554ac372f5019
cjrcjrdl1 <cjrcjrdl1@naver.com>
2023-08-25 10:54:47 +0900

    view fix

--- shop/shop/src/main/java/com/min/shop/web/HomeController.java
+++ shop/shop/src/main/java/com/min/shop/web/HomeController.java
@@ -4,6 +4,7 @@ import com.min.shop.web.member.MemberResponseDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
